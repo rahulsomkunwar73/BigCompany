ce9f1a7c98dc3869997476dd26d492fc86231404
rsomkunwar <rahul.somkunwar@cj.com>
2025-05-08 01:19:57 +0530

    CsvEmployeeRepository GetCEOData | Red

--- src/test/java/org/example/repository/CsvEmployeeRepositoryTest.java
+++ src/test/java/org/example/repository/CsvEmployeeRepositoryTest.java
@@ -8,8 +8,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.util.List;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class CsvEmployeeRepositoryTest {
 
@@ -26,4 +25,30 @@ public class CsvEmployeeRepositoryTest {
         assertNotNull(employees);
         assertFalse(employees.isEmpty());
     }
+
+    @Test
+    public void testCEOData() throws IOException {
+        // Arrange
+        String filePath = "src/test/java/org/example/resources/csvfile.csv";
+        EmployeeRepository repository = new CsvEmployeeRepository(filePath);
+
+        // Act
+        List<Employee> employees = repository.getAllEmployees();
+
+        // Assert
+
+        Employee ceo = employees.stream()
+                .filter(e -> e.getId().equals("123"))
+                .findFirst()
+                .orElse(null);
+
+        assertNotNull(ceo);
+        assertEquals("Joe", ceo.getFirstName());
+        assertEquals("Doe", ceo.getLastName());
+        assertEquals(60000, ceo.getSalary());
+        assertNull(ceo.getManagerId());
+
+    }
+
+
 }
