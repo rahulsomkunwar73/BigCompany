fe22f4712e293b11bd146a4e9c166ae891f353ef
rsomkunwar <rahul.somkunwar@cj.com>
2025-05-09 01:30:58 +0530

    EmployeeAnalyzerServiceImplTest findManagersEarningLessThanRequired | Green

--- src/main/java/org/example/service/EmployeeAnalyzerService.java
+++ src/main/java/org/example/service/EmployeeAnalyzerService.java
@@ -2,11 +2,14 @@ package main.java.org.example.service;
 
 import main.java.org.example.model.Employee;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface EmployeeAnalyzerService {
 
-   List<Employee> findManagersEarningLessThanRequired();
+   List<Employee> findManagersEarningLessThanRequired() throws IOException;
+
+   double calculateUnderpaymentAmount(Employee manager);
 
 
 }
--- src/main/java/org/example/service/EmployeeAnalyzerServiceImpl.java
+++ src/main/java/org/example/service/EmployeeAnalyzerServiceImpl.java
@@ -2,19 +2,50 @@ package main.java.org.example.service;
 
 import main.java.org.example.model.Employee;
 import main.java.org.example.repository.CsvEmployeeRepository;
+import main.java.org.example.repository.EmployeeRepository;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class EmployeeAnalyzerServiceImpl implements EmployeeAnalyzerService {
 
-    CsvEmployeeRepository csvEmployeeRepository;
+    EmployeeRepository csvEmployeeRepository;
+    private static final double MIN_SALARY_FACTOR = 1.2; // 20% more
 
-    public EmployeeAnalyzerServiceImpl(CsvEmployeeRepository csvEmployeeRepository){
+
+    public EmployeeAnalyzerServiceImpl(EmployeeRepository csvEmployeeRepository){
         this.csvEmployeeRepository = csvEmployeeRepository;
     }
 
     @Override
-    public List<Employee> findManagersEarningLessThanRequired() {
-        return null;
+    public List<Employee> findManagersEarningLessThanRequired() throws IOException {
+        List<Employee> underpaidManagers = new ArrayList<>();
+        Map<String, Employee> employeeMap = csvEmployeeRepository.buildOrganizationalStructure();
+
+        for (Employee employee : employeeMap.values()) {
+            if (employee.isManager() && calculateUnderpaymentAmount(employee) > 0) {
+                underpaidManagers.add(employee);
+            }
+        }
+
+        return underpaidManagers;
+    }
+
+    @Override
+    public double calculateUnderpaymentAmount(Employee manager) {
+        if (!manager.isManager()) {
+            return 0;
+        }
+
+        double avgSubordinateSalary = manager.getSubordinatesAverageSalary();
+        double minRequiredSalary = avgSubordinateSalary * MIN_SALARY_FACTOR;
+
+        if (manager.getSalary() < minRequiredSalary) {
+            return minRequiredSalary - manager.getSalary();
+        }
+
+        return 0;
     }
 }
